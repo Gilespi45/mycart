8d7859295a5554e94c44f9cd036502397f2e556b
Gelespi <gilespikokya45@gmail.com>
2023-08-18 22:33:29 +0530

    api

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://mycart-vercel.vercel.app',
+  origin: ['https://mycart-vercel.vercel.app'],
+  methods: ["POST","GET","PUT","DELETE"],
   credentials: true,
 }));
 app.use(express.json());
