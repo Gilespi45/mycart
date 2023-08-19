caf18d00bbbe6f05f3f37a2aa5700822b8f9b978
Gelespi <gilespikokya45@gmail.com>
2023-08-19 12:49:43 +0530

    vercel.json

--- server/server.js
+++ server/server.js
@@ -25,6 +25,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
 
 
 const User = require("./models/User");
